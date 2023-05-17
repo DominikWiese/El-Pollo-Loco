96d287ba0c7ceb9c572306fc75c37b3c3deba26e
DominikWiese <dominikwiese81@gmail.com>
2023-05-17 23:39:46 +0200

    first commit

---
+++ models/movable-object.class.js
@@ -0,0 +1,24 @@
+class MovableObject {
+
+
+    x = 120;
+    y = 250;
+    img;
+    width = 100;
+    hight = 150;
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+
+    moveRight() {
+        
+    }
+
+    moveLeft() {
+        
+    }
+}
